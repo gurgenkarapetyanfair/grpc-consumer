0290957f3b4a3cd11d6748b7bcdd57133d4addc5
gurgenkarapetyanfair <gurgen.karapetyan@o.fair.com>
2021-12-13 18:10:12 +0400

    Initial commit

---
+++ spec/grpc/consumer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Grpc::Consumer do
+  it "has a version number" do
+    expect(Grpc::Consumer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
